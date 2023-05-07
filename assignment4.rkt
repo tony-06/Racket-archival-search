a5f885c5a9d1615ee11cfc85419eb60550dbe312
Tony Russell <36004639+tony-06@users.noreply.github.com>
2023-05-06 23:16:18 -0500

    Update assignment4.rkt

--- assignment4.rkt
+++ assignment4.rkt
@@ -1,13 +1,5 @@
 #lang racket
 
-(define (clean-file file) ;remove special characters and stop words
-  (define (replace-chars string) ;helper function to replace non alphanumeric numbers
-    (regexp-replace* #px"[^[:alpha:][:digit:][:space:]]" string "")) ;Stole this from chatgpt
-  (define (remove-stop-words string-lst) ;helper function to remove stop words
-    (remove* (string-split (port->string (open-input-file "stop_words_english.txt"))) string-lst))
-  (remove-stop-words ; returns list of clean substrings
-   (string-split (string-downcase(replace-chars (port->string (open-input-file file))))))) 
-
 (define files ;creates list of files names from 001 to 025 (too lazy to write list myself)
   (map (lambda (x) (string-append (format "~a" (~r x #:min-width 3 #:pad-string "0")) ".txt"))
        (range 1 26)))
@@ -16,34 +8,26 @@
   (make-immutable-hash (map (lambda (x y) (cons x y)) keys data)))
 
 (define (master-hash-maker filenames)
+  (define (clean-file file) ;remove special characters and stop words
+    (define (replace-chars string) ;helper function to replace non alphanumeric numbers
+      (regexp-replace* #px"[^[:alpha:][:digit:][:space:]]" string "")) ;Stole this from chatgpt
+    (define (remove-stop-words string-lst) ;helper function to remove stop words
+      (remove* (string-split (port->string (open-input-file "stop_words_english.txt"))) string-lst))
+    (remove-stop-words ; returns list of clean substrings
+     (string-split (string-downcase(replace-chars (port->string (open-input-file file)))))))
   (define (make-string-hash string-lst) ;this makes my assignment 2 function look bloated
     (define count-words (map (lambda (x) (cons x (count (curry equal? x) string-lst))) string-lst))
     (define total-words (apply + (map cdr count-words)))
     (define frequencies
       (map (lambda (p) (cons (car p) (- 10(* -1 (log (/ (cdr p) total-words) 10))))) count-words))
     (make-immutable-hash frequencies))
-  (define frequencies ;creates a list of word-frequency pairs
+  (define frequency-hash ;creates a list of word-frequency pairs
     (map (lambda (file) (make-string-hash (clean-file file))) filenames))
-  (hash-maker files frequencies))
-
-(define (first-line-hash) ;hashed the first line of each file to make it return faster
-  (define first-lines
-    (map (lambda (file) (with-input-from-file file (lambda () (read-line)))) files))
-  (hash-maker files first-lines))
-
-(define (save-to-file hash name)
-  (with-output-to-file name
-    (lambda () (write hash))))
-
-(define (load-from-file filename)
-  (with-input-from-file filename
-    (lambda () (read))))
+  (hash-maker files frequency-hash))
 
 (define (search-master keywords master) ;search function for hash of hashes with a single search key
   (define (search-single hash keyword) ;single hashmap, single keyword
-    (if (hash-has-key? hash keyword)
-        (hash-ref hash keyword)
-        #f))
+    (if (hash-has-key? hash keyword) (hash-ref hash keyword) #f))
   (define (search-multiple-keywords hash keywords) ;multiple keywords, single hashmap, returns score
     (define result (map (lambda (k) (search-single hash k)) keywords))
     (define (found-keywords count r)
@@ -56,6 +40,7 @@
   (filter-sort-pairs (search-all master keywords)))
 
 (define (search)
+  (define (load-from-file filename) (with-input-from-file filename (lambda () (read))))
   (display "Enter search keywords: ")
   (define results (search-master (string-split (read-line)) (load-from-file "master-hash-file.txt")))
   (if (empty? results)
@@ -67,6 +52,11 @@
   (search))
 
 (define (main) ;creating the master-hash-file is very slow, don't do it unless you have to
+  (define (first-line-hash) ;hashed the first line of each file to make it return faster
+    (define first-lines
+      (map (lambda (file) (with-input-from-file file (lambda () (read-line)))) files))
+    (hash-maker files first-lines))
+  (define (save-to-file hash name) (with-output-to-file name (lambda () (write hash))))
   (cond
     [(and (file-exists? "master-hash-file.txt") (file-exists? "first-line-file.txt")) (search)]
     [(file-exists? "master-hash-file.txt")
